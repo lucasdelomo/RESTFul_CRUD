7f73c2322261a7e7b484fc7606136717af9c0f72
Lucas Delomo <lucasdelomo@gmail.com>
2021-10-13 20:34:52 -0300

    Movies routes set

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+import moviesRoutes from './routes/movies.js';
 
 const app = express();
 const PORT = "5000";
 
 app.use(express.json());
 
+app.use('/movies', moviesRoutes);
+
+app.get('/', (req, res) => res.send('Nada para ver aqui u.u'));
+
 app.listen(PORT, () => console.log(`Servidor em execução em http://localhost:${PORT}`));
+
